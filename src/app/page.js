514325bef96eb5a595fdbd9190a063b6076ad51d
Suriya prakash <uriyaprakash@gmail.com>
2025-03-14 17:00:05 +0530

    After sequential changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ModalPopup from "@/component/ModalPopup";
 import Header from "@/component/Header";
@@ -8,27 +9,25 @@ import NightCrackers from "@/component/NightCrackers";
 import DayCrackers from "@/component/DayCrackers";
 import Footer from "@/component/Footer";
 import "@/app/page.module.css";
-// import { Link } from "react-router-dom";
 
 export default function Home() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  useEffect(() => {
+    if (currentStep < 6) {
+      const timer = setTimeout(() => setCurrentStep(currentStep + 1), 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [currentStep]);
+
   return (
     <>
-      <Header />
-      <Banner />
-      <NightCrackers />
-      <DayCrackers />
-      <Footer />
-      <ModalPopup />
-      {/* <Link to="/quickorder"> */}
-      <div className="floating-icon" style={{ position: 'fixed', bottom: '10%', right: '3%', display: 'flex', alignItems: 'center', justifyContent: 'start', width: '18%', borderRadius: '0px' }}>
-        <div className="floating-gif" style={{ width: '32%', borderRadius: '50px', zIndex: '666' }}>
-          <a href="/quickorder"><img src={"/assets/diwali-diwali-crackers.gif"} alt="gif" style={{ width: '89%' }} /></a>
-        </div>
-        <div className="floating-text" style={{ background: '#b00', marginLeft: '-20px', borderRadius: '3px', marginTop: '5px' }}>
-          <a href="/quickorder" style={{ textDecoration: 'none' }}><p style={{ fontSize: '20px', color: 'white', padding: '8px 30px', marginBottom: '0px', fontWeight: '600' }}>Quick Order</p></a>
-        </div>
-      </div>
-      {/* </Link> */}
+      {currentStep >= 1 && <Header />}
+      {currentStep >= 2 && <Banner />}
+      {currentStep >= 3 && <NightCrackers />}
+      {currentStep >= 4 && <DayCrackers />}
+      {currentStep >= 5 && <Footer />}
+      {currentStep >= 6 && <ModalPopup />}
     </>
   );
 }
